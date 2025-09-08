efc9eee6b02a76b5f6df721ad5efa706a9d0184b
uchida189 <130959298+uchida189@users.noreply.github.com>
2025-09-08 13:07:46 +0900

    [feat]user画面にPWAを導入

--- user/vite.config.ts
+++ user/vite.config.ts
@@ -1,8 +1,29 @@
 import { defineConfig } from 'vite'
+import { VitePWA } from 'vite-plugin-pwa'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['vite.svg'],
+      manifest: {
+        name: 'My App',
+        short_name: 'App',
+        start_url: '.',
+        display: 'standalone',
+        background_color: '#ffffff',
+        icons: [
+          {
+            src: 'logo.png',
+            sizes: '320x320',
+            type: 'image/png',
+          }
+        ]
+      }
+    })
+  ],
   server: {
     host: '0.0.0.0',
     port: 5173,
