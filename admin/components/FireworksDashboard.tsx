306ea2c3f5c58fb506e3c938b04ec59110f3cf03
ponyo1114 <kakesou1114@gmail.com>
2026-06-01 19:34:29 +0900

    fix:use clientの追加

--- admin/components/FireworksDashboard.tsx
+++ admin/components/FireworksDashboard.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import React, { useState, useEffect, useCallback } from 'react';
 import FireworksErrorBanner from './FireworksErrorBanner';
 import FireworksList from './FireworksList';
